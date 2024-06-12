bc19adbe47b5f29e92300a500afa928deef00d78
EdouardY <yuedouard@gmail.com>
2024-06-12 11:00:20 +0200

    read me

---
+++ README.md
@@ -0,0 +1,22 @@
+# Backend for Lysep
+
+## Used technologies
+
+Java (Spring Boot, Selenium), Docker, 
+
+## Prerequisites
+
+Install Java (+v18) and Docker Daemon (can be Docker Desktop for instance)
+
+## How to run the backend
+
+- Launch the Docker Daemon
+- Open the project and go inside the folder src/main/resources then type docker-compose up smtp4dev
+  (Alternatively you can click on the arrow in IntelliJ)
+  ![img_1.png](imagesReadme/img_1.png)
+
+- Then on IntelliJ open the file pom.xml and click on Reload All Maven Projects
+![img.png](imagesReadme/img.png)
+
+- Then go the file src/java/software.engineering.lysep/LysepApplication and click on 'Run'
+![img2.png](imagesReadme/img2.png)
--- src/test/java/software/engineering/lysep/selenium/SeleniumTest.java
+++ src/test/java/software/engineering/lysep/selenium/SeleniumTest.java
@@ -16,13 +16,14 @@ public class SeleniumTest {
         driver.get("http://localhost:5173/authentication");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        Thread.sleep(3000);
+        // Login with Student:
+        Thread.sleep(6000);
         WebElement connexionButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='homepage-button login']")));
         connexionButton.click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='email']")));
 
-        Thread.sleep(3000);
+        Thread.sleep(6000);
         WebElement emailField = driver.findElement(By.xpath("//input[@name='email']"));
         emailField.sendKeys("edouard.yu@eleve.isep.fr");
 
@@ -35,13 +36,13 @@ public class SeleniumTest {
         loginButton.click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h2[normalize-space()='Hey dear user!']")));
-        Thread.sleep(3000);
+        Thread.sleep(6000);
 
         WebElement profilLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/a[1]")));
         profilLink.click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[normalize-space()='Mon profil']")));
-        Thread.sleep(3000);
+        Thread.sleep(6000);
 
         WebElement editButton = driver.findElement(By.xpath("//button[normalize-space()='Edit']"));
         editButton.click();
@@ -93,23 +94,23 @@ public class SeleniumTest {
         WebElement calendarLink = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/a[2]")));
         calendarLink.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[normalize-space()='Agenda']\n")));
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
         WebElement weekButton = driver.findElement(By.xpath("//button[normalize-space()='Week']"));
         weekButton.click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
         WebElement agendaButton = driver.findElement(By.xpath("//button[normalize-space()='Agenda']"));
         agendaButton.click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
         WebElement monthButton = driver.findElement(By.xpath("//button[normalize-space()='Month']"));
         monthButton.click();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
         WebElement event = driver.findElement(By.xpath("//div[@title='Électronique/Signal Project (APP) - Projet']"));
         event.click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
         WebElement closeButton = driver.findElement(By.xpath("//button[@class='modal-button modal-button-cancel']"));
         closeButton.click();
@@ -117,16 +118,65 @@ public class SeleniumTest {
 
         WebElement notificationsLink = driver.findElement(By.xpath("//*[@id=\"root\"]/div/a[3]"));
         notificationsLink.click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
         WebElement alertsLink = driver.findElement(By.xpath("//*[@id=\"root\"]/div/a[4]"));
         alertsLink.click();
-        Thread.sleep(3000);
+        Thread.sleep(5000);
 
         WebElement logoutLink = driver.findElement(By.xpath("//span[contains(text(),'Déconnexion')]"));
         logoutLink.click();
         Thread.sleep(1000);
 
+        // Login with professor
+        connexionButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@class='homepage-button login']")));
+        connexionButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='email']")));
+
+        Thread.sleep(1000);
+        emailField = driver.findElement(By.xpath("//input[@name='email']"));
+        emailField.sendKeys("admin@lysep.fr");
+
+        Thread.sleep(1000);
+        passwordField = driver.findElement(By.xpath("//input[@name='password']"));
+        passwordField.sendKeys("Admin123!");
+
+        Thread.sleep(1000);
+        loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[normalize-space()='Agenda']\n")));
+        Thread.sleep(1000);
+
+        WebElement newEvent = driver.findElement(By.xpath("//div[@title='Électronique des objets - Examen']"));
+        newEvent.click();
+        Thread.sleep(10000);
+
+        closeButton = driver.findElement(By.xpath("//button[@class='modal-button modal-button-cancel']"));
+        closeButton.click();
+        Thread.sleep(1000);
+
+        event = driver.findElement(By.xpath("//div[@title='Électronique/Signal Project (APP) - Projet']"));
+        event.click();
+        Thread.sleep(6000);
+
+        WebElement editEventButton = driver.findElement(By.xpath("//button[@class='modal-button modal-button-save']"));
+        editEventButton.click();
+        Thread.sleep(6000);
+
+        WebElement eventDescriptionField = driver.findElement(By.xpath("//input[@name='description']"));
+        eventDescriptionField.clear();
+        eventDescriptionField.sendKeys("Séance n°2");
+        Thread.sleep(6000);
+
+        WebElement saveEventButton = driver.findElement(By.xpath("//button[@class='modal-button modal-button-save']"));
+        saveEventButton.click();
+        Thread.sleep(1000);
+
+        closeButton = driver.findElement(By.xpath("//button[@class='modal-button modal-button-cancel']"));
+        closeButton.click();
+        Thread.sleep(1000);
 
         driver.quit();
     }
